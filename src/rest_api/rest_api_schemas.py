f63dc74094b5d7fa0fca1f35e2054e02503ccf0f
lazyTurtle21 <kholod@ucu.edu.ua>
2020-06-11 02:31:00 +0300

    add REST API schemas for stats queries

--- src/rest_api/rest_api_schemas.py
+++ src/rest_api/rest_api_schemas.py
@@ -8,7 +8,7 @@ class ListExistingDomainsArgs(ma.Schema):
 
 class ListExistingDomainsResponse(ma.Schema):
     domain = ma.fields.String(description='Domain name')
-    domain_list = ma.fields.List(domain, description="List of domains for which pages were created")
+    domain_list = ma.fields.List(domain, description='List of domains for which pages were created')
 
 
 class PagesByUserArgs(ma.Schema):
@@ -17,7 +17,7 @@ class PagesByUserArgs(ma.Schema):
 
 class PagesByUserResponse(ma.Schema):
     page_id = ma.fields.Integer(description='Page ID')
-    page_list = ma.fields.List(page_id, description="List of pages id for specified user_id")
+    page_list = ma.fields.List(page_id, description='List of pages id for specified user_id')
 
 
 class PagesByDomainArgs(ma.Schema):
@@ -25,8 +25,8 @@ class PagesByDomainArgs(ma.Schema):
 
 
 class PagesByDomainResponse(ma.Schema):
-    domain = ma.fields.String(description='Domain')
-    number_of_pages = ma.fields.Integer(description="Number of created pages for specified domain")
+    domain = ma.fields.String(description='Domain name')
+    number_of_pages = ma.fields.Integer(description='Number of created pages for specified domain')
 
 
 class PageByIdArgs(ma.Schema):
@@ -35,10 +35,10 @@ class PageByIdArgs(ma.Schema):
 
 class PageByIdResponse(ma.Schema):
     page_id = ma.fields.Integer(description='Page ID')
-    url = ma.fields.String(description="Url of the page")
-    title = ma.fields.String(description="Title of the page")
-    domain = ma.fields.String(description="Domain of the page")
-    namespace = ma.fields.Integer(description="Namespace of the page")
+    url = ma.fields.URL(description='Url of the page')
+    title = ma.fields.String(description='Title of the page')
+    domain = ma.fields.String(description='Domain name')
+    namespace = ma.fields.Integer(description='Namespace of the page')
 
 
 class UsersArgs(ma.Schema):
@@ -52,5 +52,38 @@ class UsersArgs(ma.Schema):
 
 class UsersResponse(ma.Schema):
     id = ma.fields.Integer(description='User ID')
-    name = ma.fields.String(description="User name")
-    number_of_pages = ma.fields.Integer(description="Number of created pages for specified user")
+    name = ma.fields.String(description='User name')
+    number_of_pages = ma.fields.Integer(description='Number of created pages for specified user')
+
+
+class CreatedPagesStats(ma.Schema):
+    statistics = ma.fields.Dict(keys=ma.fields.String(description='Domain name'),
+                                values=ma.fields.Integer(
+                                    description='Number of created pages for domain'))
+
+
+class CreatedPagesDomainsResponse(ma.Schema):
+    time_start = ma.fields.String(description='From time')
+    time_end = ma.fields.String(description='To time')
+    statistics = ma.fields.List(ma.fields.Nested(CreatedPagesStats), description='Statistics for created pages')
+
+
+class PagesBots(ma.Schema):
+    domain = ma.fields.String(description='Domain name')
+    created_by_bots = ma.fields.Integer(description='Number of pages for the domain that are created by bots')
+
+
+class CreatedPagesBotsResponse(ma.Schema):
+    time_start = ma.fields.String(description='From time')
+    time_end = ma.fields.String(description='To time')
+    statistics = ma.fields.List(ma.fields.Nested(PagesBots),
+                                description='Number of created pages by bots for domain')
+
+
+class UsersByPagesResponse(ma.Schema):
+    time_start = ma.fields.String(description='From time')
+    time_end = ma.fields.String(description='To time')
+    user_name = ma.fields.String(description='User name')
+    user_id = ma.fields.String(description='User ID')
+    page_titles = ma.fields.List(ma.fields.String(description='Page title'))
+    number_of_pages = ma.fields.Integer(description='Number of created pages for user')
